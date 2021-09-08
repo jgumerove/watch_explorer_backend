330054f791d33e3472531eb17d8899672edb3514
jgumerove <josh.gumerove@gmail.com>
2021-09-08 15:33:44 -0400

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Company.create(name: "Rolex", logo: "https://image.shutterstock.com/image-photo/kiev-ukraine-may-13-2015-260nw-278633477.jpg")
+Company.create(name: "Rolex", logo: "https://i1.wp.com/www.dafontfree.co/wp-content/uploads/2021/08/Rolex-Logo-Font-1.jpg?fit=1200%2C775&ssl=1")
 Company.create(name: "Breitling", logo: "https://moneyinc.com/wp-content/uploads/2020/09/Breitling-Logo.jpg")
 Company.create(name: "IWC Schaffhausen", logo: "https://1000logos.net/wp-content/uploads/2018/10/IWC-Logo.jpg")
-Company.create(name: "Omega", logo: "https://lh3.googleusercontent.com/proxy/0STBnvod_nPEbRRFMuR1-XbLV_rJ5lJxRRIuTh1O7KZt6WT7RjKPvOkvqPMrxM26dCMgbbGDvFWWqC9aTcGV5d1pYBFIRV1WoFS9SkNoAF_KkQ")
+Company.create(name: "Omega", logo: "https://1000logos.net/wp-content/uploads/2018/10/Omega-logo-watch.jpg")
 
 Watch.create(image_url: "https://www.bobswatches.com/images/zRolex-Cellini-50505-143089.png", collection:"Cellini", year:2019, watch_type:"Dress", band_material:"Leather", price:14995, movement:"Automatic", company_id:1)
 Watch.create(image_url: "https://k8q7r7a2.stackpathcdn.com/wp-content/uploads/2018/03/Omega-Seamaster-Diver-300M-Baselworld-2018.jpg", collection:"Seamaster", year:2018, watch_type:"Dive", band_material:"Steel", price:4575, movement:"Automatic", company_id:4)
