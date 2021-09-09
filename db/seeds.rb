f6426e61c1334cff8cd4efe282e111ee951f8e27
jgumerove <josh.gumerove@gmail.com>
2021-09-09 13:25:39 -0400

    add seed data for companies

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,13 @@ Company.create(name: "Rolex", logo: "https://i1.wp.com/www.dafontfree.co/wp-cont
 Company.create(name: "Breitling", logo: "https://moneyinc.com/wp-content/uploads/2020/09/Breitling-Logo.jpg", description: "Founded in 1884 by Leon Breitling in Saint-Imier, Breitling is a Swiss luxury watchmaker based in Grenchen, Switzerland.")
 Company.create(name: "IWC Schaffhausen", logo: "https://1000logos.net/wp-content/uploads/2018/10/IWC-Logo.jpg", description: "Founded by American watchmaker Florentine Ariosto Jones in 1868, IWC Schaffhausen is a luxury Swiss watch manufacturer located in Schaffhausen, Switzerland.")
 Company.create(name: "Omega", logo: "https://1000logos.net/wp-content/uploads/2018/10/Omega-logo-watch.jpg", description: "Founded by Louis Brandt in La Chaux-de-Fonds in 1848, Omega is a Swiss luxury watchmaker based in Biel/Bienne, Switzerland.")
+Company.create(name: "NOMOS Glashutte", logo: "https://oracleoftime.com/wp-content/uploads/2018/08/Nomos-Logo-1280x720.jpg", description: "Founded in January 1990 by Roland Shwertner, NOMOS Glashutte is a Germany watchmaking company based in Glashutte, Saxony.")
+Company.create(name: "A.Lange & Sohne", logo: "https://oracleoftime.com/wp-content/uploads/2018/12/A-Lange-Sohne-Logo.jpg", description: "Founded in 1845 by Ferdinand Adolph Lange, A. Lange & Sohne is a German manufacturer of luxury and prestige watches headquartered in Glashutte, Germany.")
+Company.create(name: "Frederique Constant", logo: "https://image.isu.pub/140721085416-7baaba0a3bf3d418b68d8e435bc82873/jpg/page_1.jpg", description: "Established in 1988 by Peter Stas and Aletta Stas-Bax, Frederique Constant is a Swiss manufacture of luxury wristwatches based in Geneva, Switzerland.")
+Company.create(name: "Patek Philippe", logo: "https://wp-aws-media.s3-accelerate.amazonaws.com/2019/09/Patek-Philippe-logo2.jpg", description: "Founded in 1839, Patek Philippe is a Swiss luxury watch and clock manufacturer located in Geneva, Switzerland.")
+
+
+
 
 Watch.create(image_url: "https://www.bobswatches.com/images/zRolex-Cellini-50505-143089.png", collection:"Cellini", year:2019, watch_type:"Dress", band_material:"Leather", price:14995, movement:"Automatic", company_id:1)
 Watch.create(image_url: "https://k8q7r7a2.stackpathcdn.com/wp-content/uploads/2018/03/Omega-Seamaster-Diver-300M-Baselworld-2018.jpg", collection:"Seamaster", year:2018, watch_type:"Dive", band_material:"Steel", price:4575, movement:"Automatic", company_id:4)
