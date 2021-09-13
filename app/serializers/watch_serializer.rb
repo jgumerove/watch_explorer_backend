148f70846e6d50cebf9b117474a0a05a15e41cb5
jgumerove <josh.gumerove@gmail.com>
2021-09-13 13:06:55 -0400

    fix serializer to just include username key for watches

--- app/serializers/watch_serializer.rb
+++ app/serializers/watch_serializer.rb
@@ -1,4 +1,8 @@
 class WatchSerializer < ActiveModel::Serializer
-  attributes :id, :image_url, :collection, :year, :watch_type, :band_material, :price, :movement
-  belongs_to :user
+  attributes :id, :image_url, :collection, :year, :watch_type, :band_material, :price, :movement, :username
+  #belongs_to :user
+  belongs_to :company
+  def username
+    self.object.user.username
+  end
 end
