b91406643b9a32cde91a39f9f9fd19521406cc0a
jgumerove <josh.gumerove@gmail.com>
2021-09-13 12:11:29 -0400

    add user reference to watch table

---
+++ db/migrate/20210913161041_add_user_to_watch.rb
@@ -0,0 +1,5 @@
+class AddUserToWatch < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :watches, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_224237) do
+ActiveRecord::Schema.define(version: 2021_09_09_193314) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2021_09_08_224237) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "logo"
     t.string "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "watches", force: :cascade do |t|
